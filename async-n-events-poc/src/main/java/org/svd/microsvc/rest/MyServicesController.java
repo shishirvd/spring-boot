f1d94819d3ab76ec82d12ebcf21ada954d96fb48
Shishir <57661228+shishirvd@users.noreply.github.com>
2020-12-31 15:33:50 +0530

    Update MyServicesController.java

--- async-n-events-poc/src/main/java/org/svd/microsvc/rest/MyServicesController.java
+++ async-n-events-poc/src/main/java/org/svd/microsvc/rest/MyServicesController.java
@@ -1,6 +1,7 @@
 package org.svd.microsvc.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.svd.microsvc.util.AsyncDataProcessor;
